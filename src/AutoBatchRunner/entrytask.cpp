dea524a1f4ac8bc0d9717de5a89191fb206f3509
elphis <elphis6572@gmail.com>
2018-02-04 02:06:33 +0900

    コメントの削除

--- src/AutoBatchRunner/entrytask.cpp
+++ src/AutoBatchRunner/entrytask.cpp
@@ -1,6 +1,112 @@
 #include "entrytask.h"
 
-EntryTask::EntryTask(QObject *parent) : QObject(parent)
+EntryTask::EntryTask(QObject *parent)
+    : QObject(parent)
 {
+    //setmain worker
+    executor = new Executor();
 
+    //set internal thread
+    worker = new QThread();
+
+    //init executor const
+    executor->setLaunchedfrom(Executor::SCHEDULER);
+
+    //connect thread to doWork
+    connect(worker, &QThread::started, executor, &Executor::runProcess);
+    connect(worker, &QThread::finished, this, &EntryTask::processCompleted);
+    connect(executor, &Executor::processEnded, worker, &QThread::quit);
+    connect(executor, &Executor::processStopped, worker, &QThread::quit);
+
+    //connect executor
+    connect(executor, &Executor::processStarted, this, &EntryTask::receiveStarted);
+    connect(executor, &Executor::processEnded, this, &EntryTask::receiveEnd);
+    connect(executor, &Executor::processStopped, this, &EntryTask::receiveStopped);
+    connect(executor, &Executor::processPaused, this, &EntryTask::receivePaused);
+
+    connect(executor, &Executor::processStateUpdate, this, &EntryTask::receiveCurrent);
+    connect(executor, &Executor::processStateCount, this, &EntryTask::receiveInitCount);
+    connect(executor, &Executor::processCheckError, this, &EntryTask::receiveErrorText);
+    connect(executor, &Executor::processMessage, this, &EntryTask::receiveMessage);
+
+    //move to thread
+    executor->moveToThread(worker);
+}
+
+EntryTask::~EntryTask()
+{
+    if(worker->isRunning()){
+        worker->quit();
+        worker->wait();
+    }
+    delete worker;
+    delete executor;
+}
+
+void EntryTask::setFile(QString filepath)
+{
+    qDebug() << "EntryTask:: File Changed :" << filepath;
+//    loadfilepath = filepath;
+    executor->setProcessFile(filepath);
+}
+
+bool EntryTask::getStarted()
+{
+    return executor->getWorking();
+}
+
+bool EntryTask::getPause()
+{
+    return executor->getPaused();
+}
+
+void EntryTask::sendInput(QString message)
+{
+    if(executor->getWorking()) executor->processWrite(message);
+}
+
+void EntryTask::start()
+{
+    if(executor->getPaused()){
+        executor->setPaused(false);
+
+    }else{
+        //load static settings
+        executor->setGlobalList();
+        executor->setLocalList();
+
+        worker->start();
+    }
+}
+
+void EntryTask::pause(bool paused)
+{
+    executor->setPaused(paused);
+}
+
+void EntryTask::stop()
+{
+    executor->stopProcess();
+}
+
+void EntryTask::updateFileList(QHash<QString, int> *data)
+{
+    if(data->empty()) return;
+    QStringList need;
+
+    QHash<QString, int>::iterator i = data->begin();
+    while (i != data->end() && i.value() == 0) {
+        need.append(i.key());
+        ++i;
+    }
+
+//    QSettings settings( "./psettings.ini", QSettings::IniFormat );
+//    settings.beginGroup("TESTEXEC");
+//    executor->addInputFiles(need, settings.value("FILELOADMAX", 1).toInt());
+//    settings.endGroup();
+}
+
+void EntryTask::processCompleted()
+{
+    qDebug() << "EntryTask::processComplete";
 }
--- src/AutoBatchRunner/entrytask.h
+++ src/AutoBatchRunner/entrytask.h
@@ -1,16 +1,83 @@
 #ifndef ENTRYTASK_H
 #define ENTRYTASK_H
 
+#include <QObject>
+#include <QMutex>
+#include <QThread>
+#include "../executor/executor.h"
+
+/**
+ * @brief The EntryTask class
+ *
+ * This class is copy of autobatchscheduler/entrytask
+ */
+
 
 class EntryTask : public QObject
 {
     Q_OBJECT
 public:
     explicit EntryTask(QObject *parent = nullptr);
+    ~EntryTask();
+
+    void setFile(QString filepath);
+
+    bool getStarted();
+    bool getPause();
 
 signals:
+    //for progress
+    void processInitCount(int start, int end);
+    void processCurrent(int currentnum);
+
+    //for progress and menu
+    void processError(int currentnum);
+
+    //for textedit
+    void processErrorText(QString str);
+    void processMessage(QString, int type);
+
+    //for all ui
+    void processStarted(int runfrom);
+    void processPaused();
+    void processStopped();
+    void processEnd(int);
 
 public slots:
+    //for progress
+    void receiveInitCount(int start, int end) { emit processInitCount(start, end); }
+    void receiveCurrent(int currentnum){emit processCurrent(currentnum); }
+
+    //for progress and menu
+    void receiveError(int currentnum){emit processError(currentnum); }
+
+    //for textedit
+    void receiveErrorText(QString str){emit processErrorText(str); }
+    void receiveMessage(QString message, int type){emit processMessage(message, type); }
+    void sendInput(QString message);
+
+    //for all ui
+    void receiveStarted(int runfrom){emit processStarted(runfrom);}
+    void receivePaused(){emit processPaused();}
+    void receiveStopped(){emit processStopped();}
+    void receiveEnd(int type){emit processEnd(type);}
+
+    //from menu
+    void start();
+    void pause(bool paused);
+    void stop();
+
+    //from settest dock
+    void updateFileList(QHash<QString, int> *data); /*int indicates status*/
+
+private slots:
+    //debug message
+    void processCompleted();
+private:
+    QMutex *mutex;
+    Executor *executor;
+    QThread *worker;
+//    QString loadfilepath = "";
 };
 
 #endif // ENTRYTASK_H
