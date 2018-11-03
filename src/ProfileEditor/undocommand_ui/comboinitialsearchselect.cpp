8c7134fb2903ea4f99774b20e966e85bbc9c30a3
elphis <elphis6572@gmail.com>
2018-11-03 23:37:56 +0900

    Info内のcomboのUndoCommandを追加

---
+++ src/ProfileEditor/undocommand_ui/comboinitialsearchselect.cpp
@@ -0,0 +1,6 @@
+#include "comboinitialsearchselect.h"
+
+ComboInitialSearchSelect::ComboInitialSearchSelect()
+{
+
+}
---
+++ src/ProfileEditor/undocommand_ui/comboinitialsearchselect.h
@@ -0,0 +1,11 @@
+#ifndef COMBOINITIALSEARCHSELECT_H
+#define COMBOINITIALSEARCHSELECT_H
+
+
+class ComboInitialSearchSelect : public QUndoCommand
+{
+public:
+    ComboInitialSearchSelect();
+};
+
+#endif // COMBOINITIALSEARCHSELECT_H
