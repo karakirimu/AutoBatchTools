88a74ab1cca9794653d112e75e27d46faccdf049
karakirimu <corept634-cmlog@yahoo.co.jp>
2020-02-03 23:20:44 +0900

    [update] delete old settings

--- src/ProfileEditor/aboutpe.cpp
+++ src/ProfileEditor/aboutpe.cpp
@@ -12,17 +12,6 @@ AboutPE::AboutPE(QWidget *parent) :
     QIcon icons(":/app_icons/app_pe.ico");
     ui->icon->setPixmap(icons.pixmap(128,128));
 
-    //textに変更
-    QFile gplfile(":/license/gpl-3.0-standalone.html");
-    gplfile.open(QFile::ReadOnly);
-    ui->gpl->setHtml(QString(gplfile.readAll()));
-    gplfile.close();
-
-    QFile lgplfile(":/license/lgpl-3.0-standalone.html");
-    lgplfile.open(QFile::ReadOnly);
-    ui->lgpl->setHtml(QString(lgplfile.readAll()));
-    lgplfile.close();
-
 }
 
 AboutPE::~AboutPE()
