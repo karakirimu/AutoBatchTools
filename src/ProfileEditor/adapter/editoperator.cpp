efdc9d200013823a8f5aeeff76cb9feb9ba92bed
karakirimu <corept634-cmlog@yahoo.co.jp>
2020-02-04 22:54:54 +0900

    [fix] unexpected double '\' in paste action

--- src/ProfileEditor/adapter/editoperator.cpp
+++ src/ProfileEditor/adapter/editoperator.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2020 karakirimu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "editoperator.h"
 
 EditOperator::EditOperator(QObject *parent)
@@ -447,9 +463,16 @@ void EditOperator::pasteAction(int id)
         for(int i = 0; i < count - 1; i++){
             tmp.clear();
             tmp = static_cast<QString>(ilist.at(i)).split("\\\" \\\"");
+
             tmp.replace(0, static_cast<QString>(tmp.at(0)).remove(0,2));
             QString inner = static_cast<QString>(tmp.at(tmp.count() - 1));
             tmp.replace(tmp.count() - 1, inner.left(inner.size() - 4));
+
+            QStringList::iterator it;
+            for (it = tmp.begin(); it != tmp.end(); ++it){
+                (*it).replace("\\\\","\\");
+            }
+
             tlist.append(tmp);
         }
 
