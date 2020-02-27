381732bd82337eee815bf81b98dfc27d85a47caf
karakirimu <corept634-cmlog@yahoo.co.jp>
2020-02-27 22:58:32 +0900

    [update] add license comment

--- src/stringxmlbuilder/stringxmlbuilder.h
+++ src/stringxmlbuilder/stringxmlbuilder.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 karakirimu
+ * Copyright 2016-2020 karakirimu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@
 /**
  * @class StringSettings_xml
  * @brief QList<QStringList> short description
- * @param data
  * @details
  * data structure
  * Left Listnum:
