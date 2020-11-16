bebe1ba13fcc806704cb3c23d8e22be2ffec5f5a
karakirimu <corept634-cmlog@yahoo.co.jp>
2020-11-16 22:41:11 +0900

    [Add] To create an icon-only type processflowtable column.

---
+++ src/ProfileEditor/graphicsarea/typeicondelegate.cpp
@@ -0,0 +1,6 @@
+#include "typedescdelegate.h"
+
+TypeDescDelegate::TypeDescDelegate()
+{
+
+}
---
+++ src/ProfileEditor/graphicsarea/typeicondelegate.h
@@ -0,0 +1,12 @@
+#ifndef TYPEDESCDELEGATE_H
+#define TYPEDESCDELEGATE_H
+
+
+class TypeDescDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    TypeDescDelegate();
+};
+
+#endif // TYPEDESCDELEGATE_H
