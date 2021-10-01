c93e12737d41500926ca20005c8cdb894e173101
karakirimu <37269014+karakirimu@users.noreply.github.com>
2021-10-01 21:38:25 +0900

    [Fix] Ubuntu test

--- res/deploy/github_ubuntu.sh
+++ res/deploy/github_ubuntu.sh
@@ -24,7 +24,7 @@ mkdir "${DEPLOYDIR}/translation"
 pushd "${DEPLOYDIR}/translation"
 for file in `ls -l ../../src/translation/abt_*.ts`
 do
-  ${QTDIR}/lrelease $file
+  ../../${QTDIR}/lrelease $file
 done
 cp ../../src/translation/abt_*.qm ./
 popd
@@ -53,19 +53,19 @@ chmod +x ${DEPLOYDIR}/launch.sh
 mkdir "${DEPLOYDIR}/plugins"
 
 # Copy Qt libraries
-cp ${QTDIR}/lib/libQt6Core.so.${QTVERSION} ${DEPLOYDIR}/libQt6Core.so.${QTVERSION_MAJOR}
-cp ${QTDIR}/lib/libQt6Gui.so.${QTVERSION} ${DEPLOYDIR}/libQt6Gui.so.${QTVERSION_MAJOR}
-cp ${QTDIR}/lib/libQt6Svg.so.${QTVERSION} ${DEPLOYDIR}/libQt6Svg.so.${QTVERSION_MAJOR}
-cp ${QTDIR}/lib/libQt6Widgets.so.${QTVERSION} ${DEPLOYDIR}/libQt6Widgets.so.${QTVERSION_MAJOR}
-cp ${QTDIR}/lib/libQt6DBus.so.${QTVERSION} ${DEPLOYDIR}/libQt6DBus.so.${QTVERSION_MAJOR}
+cp ../${QTDIR}/lib/libQt6Core.so.${QTVERSION} ${DEPLOYDIR}/libQt6Core.so.${QTVERSION_MAJOR}
+cp ../${QTDIR}/lib/libQt6Gui.so.${QTVERSION} ${DEPLOYDIR}/libQt6Gui.so.${QTVERSION_MAJOR}
+cp ../${QTDIR}/lib/libQt6Svg.so.${QTVERSION} ${DEPLOYDIR}/libQt6Svg.so.${QTVERSION_MAJOR}
+cp ../${QTDIR}/lib/libQt6Widgets.so.${QTVERSION} ${DEPLOYDIR}/libQt6Widgets.so.${QTVERSION_MAJOR}
+cp ../${QTDIR}/lib/libQt6DBus.so.${QTVERSION} ${DEPLOYDIR}/libQt6DBus.so.${QTVERSION_MAJOR}
 
 # Copy Window manager files
-cp ${QTDIR}/lib/libQt6XcbQpa.so.${QTVERSION} ${DEPLOYDIR}/libQt6XcbQpa.so.${QTVERSION_MAJOR}
-cp ${QTDIR}/lib/libQt6OpenGL.so.${QTVERSION} ${DEPLOYDIR}/libQt6OpenGL.so.${QTVERSION_MAJOR}
-cp -a ${QTDIR}/plugins/platforms ${DEPLOYDIR}/platforms
-cp -a ${QTDIR}/plugins/imageformats ${DEPLOYDIR}/imageformats
+cp ../${QTDIR}/lib/libQt6XcbQpa.so.${QTVERSION} ${DEPLOYDIR}/libQt6XcbQpa.so.${QTVERSION_MAJOR}
+cp ../${QTDIR}/lib/libQt6OpenGL.so.${QTVERSION} ${DEPLOYDIR}/libQt6OpenGL.so.${QTVERSION_MAJOR}
+cp -a ../${QTDIR}/plugins/platforms ${DEPLOYDIR}/platforms
+cp -a ../${QTDIR}/plugins/imageformats ${DEPLOYDIR}/imageformats
 
 # Copy libicu libraries
-cp ${QTDIR}/lib/libicui18n.so.${ICUVERSION} ${DEPLOYDIR}/libicui18n.so.${ICUVERSION_MAJOR}
-cp ${QTDIR}/lib/libicuuc.so.${ICUVERSION} ${DEPLOYDIR}/libicuuc.so.${ICUVERSION_MAJOR}
-cp ${QTDIR}/lib/libicudata.so.${ICUVERSION} ${DEPLOYDIR}/libicudata.so.${ICUVERSION_MAJOR}
+cp ../${QTDIR}/lib/libicui18n.so.${ICUVERSION} ${DEPLOYDIR}/libicui18n.so.${ICUVERSION_MAJOR}
+cp ../${QTDIR}/lib/libicuuc.so.${ICUVERSION} ${DEPLOYDIR}/libicuuc.so.${ICUVERSION_MAJOR}
+cp ../${QTDIR}/lib/libicudata.so.${ICUVERSION} ${DEPLOYDIR}/libicudata.so.${ICUVERSION_MAJOR}
